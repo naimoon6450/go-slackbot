0d062c159305ba29efa5bee024d786e7b552946d
Naimun Siraj <naimoon6450@gmail.com>
2023-05-17 15:08:52 -0400

    Respond to pr links + bumps within thread

--- slackclient/event_handlers.go
+++ slackclient/event_handlers.go
@@ -2,9 +2,23 @@ package slackclient
 
 import (
 	"errors"
+	"math/rand"
+	"strings"
+	"time"
 
+	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 )
+
+// Fake list of users, populate with users from channel
+// var (
+// 	ListOfUsers = []string{
+// 		"U01J9QGQZ9Z",
+// 		"U01J9QGQZ9Z",
+// 		"U01J9QGQZ9Z",
+// 		"U01J9QGQZ9Z",
+// 	}
+// )
 
 func HandleSlackEvents(
 	event slackevents.EventsAPIEvent,
@@ -28,11 +42,47 @@ func HandleSlackEvents(
 }
 
 func handleMessageEvent(event *slackevents.MessageEvent, client *Client) error {
-	// do stuff with message event
+	text := strings.ToLower(event.Text)
+	userID := event.User
+	// userID := randomUserFromList(ListOfUsers)
+	eventTS := event.TimeStamp
+	threadTS := event.ThreadTimeStamp
+
+	if strings.Contains(text, "/pull/") {
+		// tag a random user within a thread to that message
+		_, _, err := client.PostMessage(
+			event.Channel,
+			slack.MsgOptionText("<@"+userID+">", false),
+			slack.MsgOptionTS(eventTS),
+		)
+
+		if err != nil {
+			return err
+		}
+	} else if strings.Contains(text, "bump") && threadTS != "" {
+		// tag a random user within a thread to that message
+		threadTS := event.TimeStamp
+		_, _, err := client.PostMessage(
+			event.Channel,
+			slack.MsgOptionText("<@"+userID+">", false),
+			slack.MsgOptionTS(threadTS),
+		)
+
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
 func handleAppMentionEvent(event *slackevents.AppMentionEvent, client *Client) error {
 	// do stuff with app mention event
 	return nil
+}
+
+func randomUserFromList(list []string) string {
+	seed := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(seed)
+	return list[r.Intn(len(list))]
 }
